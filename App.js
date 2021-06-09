fccc48a0f0f6ae9173945749acdba84f10032ca8
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-09 21:42:10 +0300

    Started making a ToDo app

--- App.js
+++ App.js
@@ -1,40 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, StyleSheet, View } from 'react-native';
+import Header from './components/header'
 
+// ToDo App
 export default function App() {
-  const [people, setPeople] = useState([
-    {name: 'Dimo', id: '1'},
-    {name: 'Peshi', id: '2'},
-    {name: 'Goshu', id: '3'},
-    {name: 'Moshu', id: '4'},
-    {name: 'Toshu', id: '5'},
-    {name: 'Vankata', id: '6'},
-    {name: 'Petran', id: '8'},
-    {name: 'Yoan', id: '9'},
-    {name: 'Jeremy', id: '10'},
-    {name: 'Johny', id: '11'},
-  ])
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((previousPeople) => {
-      return previousPeople.filter(person => person.id != id)
-    });
-  }
+  const [todos, setTodos] = useState([
+    {text: 'finish this tutorial', key: '1'},
+    {text: 'make the I app I want to do', key: '2'},
+    {text: 'find a job', key: '3'},
+    {text: 'get money', key: '4'},
+  ]);
 
   return (
     <View style={styles.container}>
-      <FlatList 
-        data={people}
-        keyExtractor={(item) => (item.id)}
-        numColumns={2}
-        renderItem={({item}) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      <Header/>
+      <View style={styles.content}>
+        {/* form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -44,16 +36,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
-  item: {
-    marginTop: 24,
-    marginHorizontal: 10,
-    padding:30,
-    backgroundColor: 'yellow',
-    fontSize: 24,
-  }
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
 });
