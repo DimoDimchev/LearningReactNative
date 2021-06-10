970c725017dfa3e7130f5aee12352c1b37e67379
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-10 21:57:32 +0300

    Added delete icon to each task. Finished ToDo app

--- components/todoitem.js
+++ components/todoitem.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function ToDoItem({ item, pressHandler }) {
     return (
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
-        </TouchableOpacity>
+
+        <View style={styles.item}>
+            <Text style={styles.text}>{item.text}</Text>
+            <TouchableOpacity onPress={() => pressHandler(item.key)}>
+                <MaterialIcons
+                    name='delete'
+                    size={18}
+                    color='#333333'
+                />
+            </TouchableOpacity>
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
     item: {
+        flexDirection: 'row',
         padding: 16,
         marginTop: 16,
         borderColor: '#bbbbbb',
         borderWidth: 2,
         borderStyle: 'dashed',
         borderRadius: 10,
+    },
+    text: {
+        marginRight: 30,
     },
 });
