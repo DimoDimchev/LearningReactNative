70fda6f463d921bb2b6b159ec60a7e8db36a90f3
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-10 23:08:23 +0300

    Added global styles for Reviews project

--- screens/home.js
+++ screens/home.js
@@ -1,19 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 // Home screen
 export default function Home() {
   
     return (
-        <View style={styles.container}>
-            <Text>Home screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Home screen</Text>
         </View>
     );
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-    },
-  });
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,19 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 // ReviewDetails screen
 export default function ReviewDetails() {
   
     return (
-        <View style={styles.container}>
-            <Text>ReviewDetails screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>ReviewDetails screen</Text>
         </View>
     );
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-    },
-  });
