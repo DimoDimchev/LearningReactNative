fd097b12424339548cc844b0f0c087654a7da5d3
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-13 22:32:32 +0300

    Customized header for stack navigation

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -6,14 +6,25 @@ import ReviewDetails from '../screens/reviewDetails'
 // adding screens to the stack navigator to be rendered afterwards
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'My favourite movies',
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Movie review details',
+        }
     }
 };
 
 // creating the stack navigator, passing the screens to it
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerStyle: {backgroundColor: '#b9cf4e', height: 90},
+        headerTintColor: '#000000',
+    }
+});
 
 export default createAppContainer(HomeStack);
