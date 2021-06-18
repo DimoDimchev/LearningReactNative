39d7656979bbc0fba75d63dd55b60d9042b2417e
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-18 22:16:36 +0300

    Added function to retrieve reviews from Firebase  via API

--- screens/home.js
+++ screens/home.js
@@ -10,6 +10,8 @@ import Card from '../shared/card';
 // importing form for adding reviews
 import ReviewForm from './reviewForm';
 
+import firebase from 'firebase';
+
 // Home screen
 export default function Home({ navigation }) {
     // function that navigates to the review details screen
@@ -17,15 +19,26 @@ export default function Home({ navigation }) {
     //     navigation.navigate('ReviewDetails');
     // };
 
+    // set up base URI for Firebase
+    const baseURI = "https://react-native-reviews-default-rtdb.europe-west1.firebasedatabase.app/";
+
+    // variable to hold all the reviews received from the database
+    let APIreviews = undefined;
+
+    // function to get all the reviews from the Firebase database
+    const getReviews = () => {
+        fetch(baseURI + 'reviews.json')
+            .then(response => response.json())
+            .then(data => {
+                APIreviews = Object.keys(data).map(key => {
+                    return {key: key, title: data[key]["title"], rating: data[key]["rating"], body: data[key]["body"]} 
+                });
+                setReviews(APIreviews);
+            })
+    };
+
     // add data to be used in the app
-    const [reviews, setReviews] = useState([
-        { title: 'Hobbit: An unexpected journey', rating: 5, body: 'Bilbo Baggins, a hobbit, is persuaded into accompanying a wizard and a group of dwarves on a journey to reclaim the city of Erebor and all its riches from the dragon Smaug.', key: '1' },
-        { title: 'Hobbit: The desolation of Smaug', rating: 4, body: 'Bilbo Baggins, a hobbit, and his companions face great dangers on their journey to Laketown. Soon, they reach the Lonely Mountain, where Bilbo comes face-to-face with the fearsome dragon Smaug.', key: '2' },
-        { title: 'Hobbit: The battle of the 5 armies', rating: 5, body: 'Bilbo fights against a number of enemies to save the life of his Dwarf friends and protects the Lonely Mountain after a conflict arises.', key: '3' },
-        { title: 'Lord Of The Rings: The fellowship of the ring', rating: 2, body: 'A young hobbit, Frodo, who has found the One Ring that belongs to the Dark Lord Sauron, begins his journey with eight companions to Mount Doom, the only place where it can be destroyed.', key: '4' },
-        { title: 'Lord Of The Rings: The two towers', rating: 1, body: 'Frodo and Sam arrive in Mordor with the help of Gollum. A number of new allies join their former companions to defend Isengard as Saruman launches an assault from his domain.', key: '5' },
-        { title: 'Lord Of The Rings: The return of the king', rating: 3, body: 'The former Fellowship members prepare for the final battle. While Frodo and Sam approach Mount Doom to destroy the One Ring, they follow Gollum, unaware of the path he is leading them to.', key: '6' },
-    ]);
+    const [reviews, setReviews] = useState(getReviews);
 
     // state for opening and closing the modal
     const [modalOpen, setModalOpen] = useState(false);
